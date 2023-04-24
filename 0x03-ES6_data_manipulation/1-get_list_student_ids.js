72be4c7ffa2dda179f2970190dcaf9e79bc60616
simanga <simacoder@hotmail.com>
2023-04-24 19:36:06 +0000

    fix task 1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line consistent-return function
+// eslint-disable-next-line consistent-return
 export default function getListStudentIds(arg) {
   if (!Array.isArray(arg)) return [];
   return arg.map((el) => el.id);
